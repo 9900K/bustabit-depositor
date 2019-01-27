295c1050ef67d6879e4d74f07f43c30d8d0cc78a
9900K <46859140+9900K@users.noreply.github.com>
2019-01-26 23:43:33 -0700

    Update generate_wallet.js

--- src/generate_wallet.js
+++ src/generate_wallet.js
@@ -1,18 +1,3 @@
-//var bip32 = require('bip32')
-//var bitcoinjs = require('bitcoinjs-lib');
-
-//var privKey = process.env.BIP32_PRIV;
-//var hdNode = bitcoinjs.HDNode.fromBase58(privKey);
-
-//var count = process.env.GENERATE_ADDRESSES ? parseInt(process.env.GENERATE_ADDRESSES) : 1000; // how many addresses to watch
-
-
-//var rescan = 'false';
-
-//for (var i = 1; i <= count; ++i) {
-//  console.log('bitcoin-cli importprivkey ' +  hdNode.derive(i).keyPair.toWIF() + " '' " + rescan)
-}
-
 var bip32 = require('bip32')
 
 var privKey = process.env.BIP32_PRIV;
