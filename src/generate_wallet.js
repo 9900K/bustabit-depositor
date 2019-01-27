cf54ce89d3ad32e9f0b583623beaca13f94be9e9
9900K <46859140+9900K@users.noreply.github.com>
2019-01-26 23:42:09 -0700

    Update generate_wallet.js

--- src/generate_wallet.js
+++ src/generate_wallet.js
@@ -1,8 +1,22 @@
+//var bip32 = require('bip32')
+//var bitcoinjs = require('bitcoinjs-lib');
+
+//var privKey = process.env.BIP32_PRIV;
+//var hdNode = bitcoinjs.HDNode.fromBase58(privKey);
+
+//var count = process.env.GENERATE_ADDRESSES ? parseInt(process.env.GENERATE_ADDRESSES) : 1000; // how many addresses to watch
+
+
+//var rescan = 'false';
+
+//for (var i = 1; i <= count; ++i) {
+//  console.log('bitcoin-cli importprivkey ' +  hdNode.derive(i).keyPair.toWIF() + " '' " + rescan)
+}
+
 var bip32 = require('bip32')
-var bitcoinjs = require('bitcoinjs-lib');
 
 var privKey = process.env.BIP32_PRIV;
-var hdNode = bitcoinjs.HDNode.fromBase58(privKey);
+var hdNode = bip32.fromBase58(privKey);
 
 var count = process.env.GENERATE_ADDRESSES ? parseInt(process.env.GENERATE_ADDRESSES) : 1000; // how many addresses to watch
 
@@ -10,5 +24,5 @@ var count = process.env.GENERATE_ADDRESSES ? parseInt(process.env.GENERATE_ADDRE
 var rescan = 'false';
 
 for (var i = 1; i <= count; ++i) {
-  console.log('bitcoin-cli importprivkey ' +  hdNode.derive(i).keyPair.toWIF() + " '' " + rescan)
+  console.log('bitcoin-cli importprivkey ' +  hdNode.derive(i).toWIF() + " '' " + rescan)
 }
